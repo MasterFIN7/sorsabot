6f0b9d5ca88498bd3775393c9b4c5c3e4a4d71df
MasterFIN7 <38255441+MasterFIN7@users.noreply.github.com>
2018-05-07 14:16:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
+let coins = require("./coins.json");
 
 fs.readdir("./commands/", (err, files) => {
 
@@ -46,6 +47,31 @@ bot.on("message", async message => {
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
+
+    if(!coins[message.author.id]){
+      coins[message.author.id] = {
+        coins: 0
+      };
+    }
+
+    let coinAmt = Math.floor(Math.random() * 15) + 1;
+    let baseAmt = Math.floor(Math.random() * 15) + 1;
+    console.log(`${coinAmt} ; ${baseAmt}`);
+
+    if(coinAmt === baseAmt){
+      coins[message.author.id] = {
+        coins: coins[message.author.id].coins + coinAmt
+      };
+      fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
+        if (err) console.log(err)
+      });
+      let coinEmbed = new Discord.RichEmbed()
+      .setAuthor(message.author.username)
+      .setColor("#0000FF")
+      .addField("💸", `${coinAmt} coins added!`);
+
+      message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+    }
 
 
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
